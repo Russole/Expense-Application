7c8eee368a8c9fd5b98cbc2af00fe2e15424483e
Russole <850905junior@gmail.com>
2025-01-09 18:18:01 +0800

    Use Expense Interface to Define the Expenses Array

--- Frontend/webapp/src/pages/dashboard/Dashboard.tsx
+++ Frontend/webapp/src/pages/dashboard/Dashboard.tsx
@@ -1,22 +1,30 @@
+import { Expense } from "../../model/Expense";
+
 const Dashboard = () => {
-  const expenses = [
+  const expenses: Expense[] = [
     {
       id: 1,
       name: "Water bill",
       amount: 288.0,
       date: new Date().toDateString(),
+      category: "Utilities",
+      note: "bulls",
     },
     {
       id: 2,
       name: "Electricity bill",
       amount: 500.0,
       date: new Date().toDateString(),
+      category: "Utilities",
+      note: "bulls",
     },
     {
       id: 3,
       name: "Wifi bill",
       amount: 700.0,
       date: new Date().toDateString(),
+      category: "Utilities",
+      note: "bulls",
     },
   ];
   return (
