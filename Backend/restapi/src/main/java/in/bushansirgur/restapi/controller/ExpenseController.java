1fcd6f9edcd71de2d399fea68917393d969cb860
Russole <850905junior@gmail.com>
2025-01-09 13:16:07 +0800

    Update the controller
    - Convert the ExpenseDTO object to ExpenseResponse object
    - Test the API endpoint

--- Backend/restapi/src/main/java/in/bushansirgur/restapi/controller/ExpenseController.java
+++ Backend/restapi/src/main/java/in/bushansirgur/restapi/controller/ExpenseController.java
@@ -1,5 +1,10 @@
 package in.bushansirgur.restapi.controller;
 
+import in.bushansirgur.restapi.dto.ExpenseDTO;
+import in.bushansirgur.restapi.io.ExpenseResponse;
+import in.bushansirgur.restapi.service.ExpenseService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,11 +12,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@RequiredArgsConstructor
 public class ExpenseController {
 
-    @GetMapping("/expenses")
-    public String getExpenses() {
+    private final ExpenseService expenseService;
+    private final ModelMapper modelMapper;
 
-        return "Reading the expenses from database";
+    @GetMapping("/expenses")
+    public List<ExpenseResponse> getExpenses() {
+        //Call the service method
+        List <ExpenseDTO >list= expenseService.getAllExpenses();
+        //Convert the Expense DTO to Expense Response
+        List<ExpenseResponse> response = list.stream().map(expenseDTO -> mapToExpenseResponse(expenseDTO)).collect(Collectors.toList());
+        //Return the list/response
+        return response;
+    }
+    private ExpenseResponse mapToExpenseResponse(ExpenseDTO expenseDTO) {
+        return modelMapper.map(expenseDTO, ExpenseResponse.class);
     }
 }
