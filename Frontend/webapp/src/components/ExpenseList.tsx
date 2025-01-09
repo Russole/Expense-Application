3337101de51bad3ed0c5906c36ca7fab6dd3325b
Russole <850905junior@gmail.com>
2025-01-09 18:25:15 +0800

    Create a Reusable Component ExpenseList

---
+++ Frontend/webapp/src/components/ExpenseList.tsx
@@ -0,0 +1,54 @@
+import { Expense } from "../model/Expense";
+
+const ExpenseList = () => {
+  const expenses: Expense[] = [
+    {
+      id: 1,
+      name: "Water bill",
+      amount: 288.0,
+      date: new Date().toDateString(),
+      category: "Utilities",
+      note: "bulls",
+    },
+    {
+      id: 2,
+      name: "Electricity bill",
+      amount: 500.0,
+      date: new Date().toDateString(),
+      category: "Utilities",
+      note: "bulls",
+    },
+    {
+      id: 3,
+      name: "Wifi bill",
+      amount: 700.0,
+      date: new Date().toDateString(),
+      category: "Utilities",
+      note: "bulls",
+    },
+  ];
+  return (
+    <div>
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Amount</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((expense) => (
+            <tr key={expense.id}>
+              <td>{expense.name}</td>
+              <td>{expense.amount}</td>
+              <td>{expense.date}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ExpenseList;
