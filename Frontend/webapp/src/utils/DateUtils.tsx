dd9a986d9d750278a3fc18e70fa65cf0b44cca77
Russole <850905junior@gmail.com>
2025-01-10 12:30:17 +0800

    Format the Date in ExpensesList Component

--- Frontend/webapp/src/utils/DateUtils.tsx
+++ Frontend/webapp/src/utils/DateUtils.tsx
@@ -31,6 +31,17 @@ class DateUtils {
 
     return `${dayName} ${day} ${month}, ${year}`;
   }
+
+  static formatDateString(dateString: string) {
+    if (dateString != undefined) {
+      const date = new Date(dateString);
+      return new Intl.DateTimeFormat("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "2-digit",
+      }).format(date);
+    }
+  }
 }
 
 export default DateUtils;
