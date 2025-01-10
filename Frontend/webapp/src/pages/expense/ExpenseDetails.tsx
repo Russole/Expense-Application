eeeddf6e1d9f52cd28f03ad73f0edbaa6d92ebb7
Russole <850905junior@gmail.com>
2025-01-10 20:03:27 +0800

    Create Basic HTML Table for Expense Detail page

--- Frontend/webapp/src/pages/expense/ExpenseDetails.tsx
+++ Frontend/webapp/src/pages/expense/ExpenseDetails.tsx
@@ -1,8 +1,41 @@
 import { useParams } from "react-router-dom";
+import CurrencyUtils from "../../utils/CurrencyUtils";
+import DateUtils from "../../utils/DateUtils";
 
 const ExpenseDetails = () => {
   const { expenseId } = useParams();
-  return <div>ExpenseDetails - {expenseId}</div>;
+  return (
+    <div className="container mt-2">
+      <div className="card">
+        <div className="card-body p-3">
+          <table className="table table-borderless table-responsive">
+            <tbody>
+              <tr>
+                <th>Name</th>
+                <td>Water bill</td>
+              </tr>
+              <tr>
+                <th>Category</th>
+                <td>Bills</td>
+              </tr>
+              <tr>
+                <th>Amount</th>
+                <td>{CurrencyUtils.formatToINR(500)}</td>
+              </tr>
+              <tr>
+                <th>Date</th>
+                <td>{DateUtils.formatDateString("2024-7-14")}</td>
+              </tr>
+              <tr>
+                <th>Note</th>
+                <td>My first spending</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseDetails;
