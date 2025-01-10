65e195cc97779abd0a18619f697aab59599a8009
Russole <850905junior@gmail.com>
2025-01-10 14:24:41 +0800

    Calculate the Total Expenses Amount

--- Frontend/webapp/src/pages/dashboard/DashboardStatus.tsx
+++ Frontend/webapp/src/pages/dashboard/DashboardStatus.tsx
@@ -1,20 +1,25 @@
 import CurrencyUtils from "../../utils/CurrencyUtils";
 import DateUtils from "../../utils/DateUtils";
 
-const DashboardStatus = () => {
+interface Props {
+  loggedInUser: string;
+  totalExpenses: number;
+}
+
+const DashboardStatus = ({ loggedInUser, totalExpenses }: Props) => {
   return (
     <div className="mt-2">
       <div className="text-center">
         <h2 className="mb-4">Total Expenses</h2>
         <h3>
           <span className="badge rounded-pill app-primary-bg-color">
-            {CurrencyUtils.formatToINR(500)}
+            {CurrencyUtils.formatToINR(totalExpenses)}
           </span>
         </h3>
       </div>
       <div className="d-flex justify-content-between">
         <div>
-          Welcome, <b className="app-primary-color">"Luffy@example.com"</b>
+          Welcome, <b className="app-primary-color">{loggedInUser}</b>
         </div>
         <div>{DateUtils.getFormattedDate(new Date())}</div>
       </div>
