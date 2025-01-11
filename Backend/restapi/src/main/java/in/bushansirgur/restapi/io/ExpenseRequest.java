7d4bda122d575435b69cfc0b9a8e197a44a473ee
Russole <850905junior@gmail.com>
2025-01-11 16:40:53 +0800

    Add More Validations

--- Backend/restapi/src/main/java/in/bushansirgur/restapi/io/ExpenseRequest.java
+++ Backend/restapi/src/main/java/in/bushansirgur/restapi/io/ExpenseRequest.java
@@ -3,6 +3,7 @@ package in.bushansirgur.restapi.io;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -15,15 +16,18 @@ import java.sql.Date;
 
 public class ExpenseRequest {
 
-    @NotBlank
+    @NotBlank(message = "Expense name is required")
+    @Size(min = 3, message = "Expense name should be at least 3 characters")
     private String name;
 
     private String note;
-    
-    @NotBlank
+
+    @NotBlank(message = "Expense category is required")
     private String category;
 
+    @NotNull(message = "Expense date is required")
     private Date date;
 
+    @NotNull(message = "Expense amount is required")
     private BigDecimal amount;
 }
