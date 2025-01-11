8ca205a6e99304cfa504e4603552797c68328184
Russole <850905junior@gmail.com>
2025-01-11 20:17:24 +0800

    Install Formik and Use it

--- Frontend/webapp/src/pages/expense/NewExpense.tsx
+++ Frontend/webapp/src/pages/expense/NewExpense.tsx
@@ -1,13 +1,36 @@
+import { useFormik } from "formik";
+import split from "./../../../node_modules/lodash-es/split";
+import { Expense } from "../../model/Expense";
+
 const NewExpense = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      amount: 0,
+      note: "",
+      category: "",
+      date: new Date().toISOString().split("T")[0],
+    },
+    onSubmit: (values: Expense) => {
+      console.log(values);
+    },
+  });
   return (
     <div className="d-flex justify-content-center align-items-center mt-2">
       <div className="container col-md-4 col-sm-8 col-xs-12">
-        <form>
+        <form onSubmit={formik.handleSubmit}>
           <div className="mb-3">
             <label htmlFor="name" className="form-label">
               Name
             </label>
-            <input type="text" id="name" name="name" className="form-control" />
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="form-control"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+            />
             <div className="text-danger fst-italic">
               Expense name is required
             </div>
@@ -21,6 +44,8 @@ const NewExpense = () => {
               id="amount"
               name="amount"
               className="form-control"
+              value={formik.values.amount}
+              onChange={formik.handleChange}
             />
             <div className="text-danger fst-italic">
               Expense amount is required
@@ -30,18 +55,34 @@ const NewExpense = () => {
             <label htmlFor="note" className="form-label">
               Note
             </label>
-            <textarea id="note" name="note" className="form-control" />
+            <textarea
+              id="note"
+              name="note"
+              className="form-control"
+              value={formik.values.note}
+              onChange={formik.handleChange}
+            />
           </div>
           <div className="mb-3">
             <label htmlFor="date" className="form-label">
               Date
             </label>
-            <input type="date" id="date" name="date" className="form-control" />
+            <input
+              type="date"
+              id="date"
+              name="date"
+              className="form-control"
+              value={formik.values.date}
+              onChange={formik.handleChange}
+            />
             <div className="text-danger fst-italic">
               Expense date is required
             </div>
           </div>
-          <button className="btn btn-sm btn-primary btn-outline-light">
+          <button
+            className="btn btn-sm btn-primary btn-outline-light"
+            type="submit"
+          >
             Save
           </button>
         </form>
