c811c2b45e5df53855a47e785bbe6b8f7de52b48
Russole <850905junior@gmail.com>
2025-01-12 09:34:30 +0800

    Create New Route to Edit Expense
    - Get the Existing Expense.
    - Display the Expense Values in Form.

--- Frontend/webapp/src/pages/expense/NewExpense.tsx
+++ Frontend/webapp/src/pages/expense/NewExpense.tsx
@@ -3,21 +3,44 @@ import { Expense } from "../../model/Expense";
 import expenseValidationSchema from "../../validation/expenseValidationSchema";
 import { expenseCategories } from "../../utils/AppConstants";
 import Dropdown from "../../components/Dropdown";
-import { saveOrUpdateExpense } from "../../services/expense-service";
+import {
+  getExpenseByExpenseId,
+  saveOrUpdateExpense,
+} from "../../services/expense-service";
 import { useNavigate, useParams } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const NewExpense = () => {
+  const { expenseId } = useParams<{ expenseId: string }>();
   const navigate = useNavigate();
   const [error, setErrors] = useState<string>("");
+  const [isLoading, setLoader] = useState<boolean>(false);
+  const [initialValues, setInitialValues] = useState<Expense>({
+    name: "",
+    amount: "",
+    note: "",
+    category: "",
+    date: new Date().toISOString().split("T")[0],
+  });
+
+  useEffect(() => {
+    if (expenseId) {
+      //call the service method to get the exising expense
+      setLoader(true);
+      getExpenseByExpenseId(expenseId)
+        .then((response) => {
+          if (response && response.data) {
+            setInitialValues(response.data);
+          }
+        })
+        .catch((error) => setErrors(error.message))
+        .finally(() => setLoader(false));
+    }
+  }, [expenseId]);
+
   const formik = useFormik({
-    initialValues: {
-      name: "",
-      amount: 0,
-      note: "",
-      category: "",
-      date: new Date().toISOString().split("T")[0],
-    },
+    initialValues: initialValues,
+    enableReinitialize: true,
     onSubmit: (values: Expense) => {
       saveOrUpdateExpense(values)
         .then((response) => {
@@ -36,6 +59,7 @@ const NewExpense = () => {
     <div className="d-flex justify-content-center align-items-center mt-2">
       <div className="container col-md-4 col-sm-8 col-xs-12">
         {error && <p className="text-danger fst-italic">{error}</p>}
+        {isLoading && <p>Loading...</p>}
         <form onSubmit={formik.handleSubmit}>
           <div className="mb-3">
             <label htmlFor="name" className="form-label">
